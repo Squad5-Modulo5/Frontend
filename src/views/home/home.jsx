cfbe7012793f3408123a33597cd7e9068e5a5c54
David <d4vidmenezes@gmail.com>
2024-05-06 16:50:14 -0300

    feat(views): Added the home view whit presentation, footer and header

---
+++ src/views/home/home.jsx
@@ -0,0 +1,17 @@
+import "./home.css"
+import Footer from "../../components/footer/footer"
+import Header from "../../components/header/header"
+import Presentation from "../../components/presentation/presentation"
+
+export default function Home (){
+    return(
+        <div>
+            <Header/>
+            <Presentation/>
+            <section></section>
+            <section></section>
+            <section></section>
+            <Footer/>
+        </div>
+    )
+}
