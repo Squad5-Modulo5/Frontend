036abb1febfc37472675830016b27c32db3e6cf3
David <d4vidmenezes@gmail.com>
2024-05-06 15:06:34 -0300

    feat(components) added footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+import ecoelektSimbulo from "../../assets/logo/ecoelekt.simbulo.svg"
+import './footer.css'
+export default function Footer (){
+    return(
+        <div className="footer">
+            <img src={ecoelektSimbulo} alt="ecoelekt-simbulo"/>
+        </div>
+    )
+}
