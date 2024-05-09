1dde89331869ad471df9e8ed1998ec275dbdc2e0
Potato0708 <tca7alvs@gmail.com>
2024-05-08 21:24:29 -0300

    constructin of section A and C

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Home from './views/home/home.jsx'
+import Home from './views/home/home'
+import SectionA from './components/sections/sectionA'
+import SectionC from './components/sections/sectionC'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
+    <SectionA/>
+    <SectionC/>
   </React.StrictMode>,
 )
