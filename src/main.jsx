68b0451cbee70509d54e55b66a0e0272a8dea219
sthefanie <sthefaniesantana1306@gmail.com>
2024-05-14 12:18:57 -0300

    sthefanie

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Home from './views/home/home.jsx'
+import SectionB from './components/sections/sectionB.jsx'
+import SectionD from './components/sections/sectionD.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
+    <SectionB/>
+    <SectionD/>
   </React.StrictMode>,
 )
