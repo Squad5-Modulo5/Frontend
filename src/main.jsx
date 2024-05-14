7b0a5bfcaf2603afa1523b4b6a5fa597e681fb8f
2D <90729142+callmetwod@users.noreply.github.com>
2024-05-14 16:25:30 -0300

    Revert "Sthefanie"

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Home from './views/home/home.jsx'
+import Home from './views/home/home'
 import SectionA from './components/sections/sectionA'
 import SectionC from './components/sections/sectionC'
-import SectionB from './components/sections/sectionB.jsx'
-import SectionD from './components/sections/sectionD.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Home/>
     <SectionA/>
     <SectionC/>
-    <SectionB/>
-    <SectionD/>
   </React.StrictMode>,
 )
