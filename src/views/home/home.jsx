dd4b8a21fb40b1e2cf49a7194b7ca6c405ecc327
Potato0708 <tca7alvs@gmail.com>
2024-06-01 17:53:07 -0300

    commit test

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -3,6 +3,7 @@ import Footer from "../../components/footer/footer"
 import Header from "../../components/header/header"
 import Presentation from "../../components/presentation/presentation"
 import SectionA from "../../components/sections/sectionA.jsx";
+
 import SectionC from "../../components/sections/sectionC.jsx";
 // eslint-disable-next-line no-unused-vars
 import React from "react";
